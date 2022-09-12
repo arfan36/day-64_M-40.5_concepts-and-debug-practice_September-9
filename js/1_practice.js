117e63c437030666703771cd41f3eeedae4dae3c
arfan36 <arfanur36@gmail.com>
2022-09-12 22:07:18 +0600

    nosto bank

---
+++ js/1_practice.js
@@ -0,0 +1,24 @@
+/* 
+@────────────────────────────────────────────────────────────── Js Concepts ─────
+1. null vs undefined
+2. == vs ===
+3. scope: block, function, global
+4. closure
+5. hoisting
+6. calback function
+7. arguments. what is array like object.
+8. array vs array like object
+9. truthy vs falsy
+10. ! vs !!
+*/
+
+/* 
+@───────────────────────────────────────────────────────────────── Dev Tool ─────
+1. Elements Tab
+2. Console Tab
+3. Source Tab
+4. Network Tab
+5. Application Tab
+6. Check Responsive
+7. Shortcut key
+ */
